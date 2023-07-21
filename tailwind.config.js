5dc86b70ea1af1697307b34a31275bf76bc266fc
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-07-21 16:04:19 -0300

    feat: configured fontsize tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.tsx'],
+  content: ["./src/**/*.tsx"],
   theme: {
-    colors:{
-      'gray-900':'#121214',
-      'gray-800':'#202024',
-      'gray-400':'#7c7c8a',
-      'gray-200':'#c4c4cc',
-      'gray-100':'#e1e1e6',
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      "gray-900": "#121214",
+      "gray-800": "#202024",
+      "gray-400": "#7c7c8a",
+      "gray-200": "#c4c4cc",
+      "gray-100": "#e1e1e6",
 
-      'cyan-500':'#81d8f7',
-      'cyan-300':'#9BE1fB'
+      "cyan-500": "#81d8f7",
+      "cyan-300": "#9BE1fB",
     },
     extend: {
-      fontFamily:{
-        sans:'Inter, sans-serif'
-      }
+      fontFamily: {
+        sans: "Inter, sans-serif",
+      },
     },
   },
   plugins: [],
-}
-
+};

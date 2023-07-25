ec9a438cb3dfe248028c4fee36cb4a7e5f5670ba
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-07-25 17:25:35 -0300

    fix: added argTypes

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -6,6 +6,15 @@ export default {
   component: Text,
   args: {
     children: "Lorem ipsum",
+    size: "md",
+  },
+  argTypes: {
+    size: {
+      options: ["sm", "md", "lg"],
+      control: {
+        type: "inline-radio",
+      },
+    },
   },
 } as Meta<TextProps>;
 
@@ -25,5 +34,12 @@ export const CustomComponent: StoryObj<TextProps> = {
   args: {
     asChild: true,
     children: <p>Testando</p>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
   },
 };
